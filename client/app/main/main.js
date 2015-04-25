26d3a216002f3c100edd39bbda77e6815a4e52ad
Camilo Giraldo <camilogiraldo91@gmail.com>
2015-04-25 15:49:40 -0500

    Login

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('udeacademicApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
